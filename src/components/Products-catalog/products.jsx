4fca6b67a9e2c9778fc85fdafacd6f8d3af83777
Maranyil <mara.mulato@gmail.com>
2020-09-06 04:29:13 -0500

    pretty catalog with mock carousel

--- src/components/Products-catalog/products.jsx
+++ src/components/Products-catalog/products.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -44,12 +45,14 @@ const Products = () => {
       <ul className="wrapper">
           {products.map((item) => (
             <Card className={classes.root} key={item.id}>
+              <Link to="/product-info">
               <CardMedia
                 className={classes.media}
                 image={item.image}
               ></CardMedia>
               <Typography variant="h6">{item.name}</Typography>
               <Typography variant="body2" color="primary">$ {item.price}.00</Typography>
+              </Link>
             </Card>
           ))}
         
